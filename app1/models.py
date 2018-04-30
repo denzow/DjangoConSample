cd0af7df0c4c28bfa3b905e58879652d1d2a2ccb
denzow <denzow@gmail.com>
2018-04-30 16:06:55 +0900

    [add] dummy model

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
